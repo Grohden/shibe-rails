9ebdca42bfa074dab2546f3c045f6fd56dda40f8
gabriel.rohden <gabriel_rohden@outlook.com>
2022-04-08 14:35:44 -0300

    feat: animal pictures by id query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 module Types
   class QueryType < Types::BaseObject
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :animal_pictures_by_id, Types::AnimalPictureType, null: false do
+      argument :picture_id, ID, required: true
+    end
+    def animal_pictures_by_id(picture_id:)
+      AnimalPicture.find(picture_id)
     end
   end
 end
