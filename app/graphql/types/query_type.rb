2dce7a3efb8c89a94f27e5f5b3d120a30d5cf51c
gabriel.rohden <gabriel_rohden@outlook.com>
2022-04-08 14:37:42 -0300

    feat: animal pictures by user tag query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,5 +8,15 @@ module Types
     def animal_pictures_by_id(picture_id:)
       AnimalPicture.find(picture_id)
     end
+
+    field :animal_pictures_by_user_tag, Types::AnimalPictureType.connection_type, null: false do
+      argument :tag_id, ID, required: true
+      argument :user_id, ID, required: true
+    end
+    def animal_pictures_by_user_tag(tag_id:, user_id:)
+      AnimalPicture
+        .joins(:tags)
+        .where(tags: { user_id: user_id }, picture_tags: { tag_id: tag_id })
+    end
   end
 end
